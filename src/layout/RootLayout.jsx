2e6f86958d5b4b67cb5fe80b6f48878946bffd87
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-17 10:37:42 +0900

    RootLayout

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import MainNavigation from './MainNavigation'
+
+const RootLayout = () => {
+  return (
+    <>
+    <MainNavigation/>
+    <main>
+      {/* router에서 설정한 children page들이 Outlet에 렌더링됨*/}
+    <Outlet/>
+    </main>
+    </>
+  )
+}
+
+export default RootLayout
