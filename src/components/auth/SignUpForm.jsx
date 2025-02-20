b883b3c23d0d960af774d6b01e41ec34c8c1cc74
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-20 12:00:47 +0900

    애니메이션 추가

--- src/components/auth/SignUpForm.jsx
+++ src/components/auth/SignUpForm.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import styles from './SignUpForm.module.scss';
-import EmailInput from './EmailInput';
+import React, { use, useState } from "react";
+import styles from "./SignUpForm.module.scss";
+import EmailInput from "./EmailInput";
+import VerificationInput from "./VerificationInput";
+import ProgressBar from "../common/ProgressBar";
 
 const SignUpForm = () => {
+  // 현재 어떤 스텝에 위치하고 잇는지
+  const [step, setStep] = useState(1);
+  // 다음 스텝으로 넘어가는 여부
+  const [success, setSucess] = useState(false);
+
+  // 이메일 중복확인이 끝났을 때 호출될 함수
+  const emailSuccessHandler = () => {
+    setSucess(true);
+    console.log("step1이 통과됨");
+    setTimeout(() => {
+      setStep(2);
+      setSucess(false);
+    }, 750);
+  };
+
   return (
     <div className={styles.signupForm}>
       <div className={styles.formStepActive}>
-
-        <EmailInput />
-
+        {step === 1 && <EmailInput onSuccess={emailSuccessHandler} />}
+        {step === 2 && <VerificationInput />}
+        {success && <ProgressBar />}
       </div>
     </div>
   );
 };
 
-export default SignUpForm;  
+export default SignUpForm;
